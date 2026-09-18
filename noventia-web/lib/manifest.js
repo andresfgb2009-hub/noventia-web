7c15853af222c8f15009fa0a214e4713892df230
andresfgb2009-hub <andresfgb2009@gmail.com>
2026-09-18 19:21:58 +0200

    eliminar

--- noventia-web/lib/manifest.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  "use strict";
-  window.__BRAND__ = {
-    name: "Noventia",
-    tagline: "IA para clínicas que no quieren perder ni una llamada más.",
-    contactEmail: "andres@noventia.tech",
-    web3formsKey: "db23fc12-8276-41b5-a3e3-0fed6c19c0bb"
-  };
-})();
